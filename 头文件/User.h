9fb8ed708e0e1c71d11bafa590e88afac1f048e5
Yxh1433 <41407555+yxh0@users.noreply.github.com>
2019-12-22 21:19:48 +0800

    Update User.h

--- 头文件/User.h
+++ 头文件/User.h
@@ -3,8 +3,8 @@
 #define USER_H
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <fstream>
+#include <map>
+#include "goods.h"
 using namespace std;
 
 class User     //用户类
@@ -13,16 +13,22 @@ protected:
 	string ID;            //用户登录ID
 	string Password;      //用户密码
 	string UserName;      //用户名
-	double User_Blance;   //用户钱包
+	string Address;       //用户地址
+	map<string, GOODS> good;
 public:
 	User();               //不带参数的构造
-	User(string _ID, string _Password, string _UserName, double _User_Blance);  //带参数的构造
-	User(User& u);        //拷贝构造
+	User(string _ID, string _Password, string _UserName);  //带参数的构造
+	User(const User& u);        //拷贝构造
 	~User() {};           //析构函数
 	void Register();      //用户注册
-	void Login();         //用户登录
-
-
+	bool Login();         //用户登录
+	friend void Search(User& s, string goods);
+	string getId();
+	void addAdd();
+	string get_name();
+	bool checkPassword();
 };
+
+
 
 #endif
